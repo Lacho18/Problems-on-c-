fb12a81116cbea7f44b9f3c538011379a043987f
Lacho18 <121165891+Lacho18@users.noreply.github.com>
2023-10-17 19:47:13 +0300

    Update Search-a-2d-Matrix.cpp

--- Problems-on-c++/Search-a-2d-Matrix/Search-a-2d-Matrix.cpp
+++ Problems-on-c++/Search-a-2d-Matrix/Search-a-2d-Matrix.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 /*
+    You are given an m x n integer matrix with the following two properties:
 
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+    Given an integer target, return true if target is in matrix or false otherwise.
 
+    You must write a solution in O(log(m * n)) time complexity.
+    
 */
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
