907d1752235cec22e90d3fc01740044b1bf1772e
Lacho18 <121165891+Lacho18@users.noreply.github.com>
2023-10-23 19:42:42 +0300

    Update Convert_Sorted_List_to_Binary_Search_Tree.cpp

--- Problems-on-c++/Convert_Sorted_List_to_Binary_Search_Tree/Convert_Sorted_List_to_Binary_Search_Tree.cpp
+++ Problems-on-c++/Convert_Sorted_List_to_Binary_Search_Tree/Convert_Sorted_List_to_Binary_Search_Tree.cpp
@@ -3,7 +3,7 @@
 /*
     Given the head of a singly linked list where elements are 
     sorted in ascending order, convert it to a height-balanced
-    binary search tree.
+    binary search tree.         
 */
 
 struct Tree {
